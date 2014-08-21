ccb87422e10488c8ebb858a77f02539de17decee
Hashem Zahran <hezahran@gmail.com>
2014-08-21 20:25:16 +0300

    Building final website file structure hierarchy

--- README.md
+++ README.md
@@ -2,16 +2,17 @@
 
 This repo contains all source files for Hashem Zahran which is based on:
 
-* [Bootstrap - v3.2.0](http://getbootstrap.com/)
-* [SCSS](http://sass-lang.com/) principles and splited layout/scss subfiles
-* [Hammer](http://hammerformac.com/) Template feature which helps creating html files out of multiple assets via "include rule"
+* [Bootstrap - v3.2.0](http://getbootstrap.com/).
+* [SCSS](http://sass-lang.com/) principles and splited layout/scss subfiles.
+* [Hammer](http://hammerformac.com/) Template feature which helps creating html files out of multiple assets via "include rule".
 
 ---- 
 
 ## Credits
 
-Due to the fact the framework is based on Bootstrap the Credits goes the the creators:
+Due to the fact this website is based based on Bootstrap and others the credits goes the the creators of:
 
-* [Bootstrap - v3.2.0](http://getbootstrap.com/)
+* [Bootstrap - v3.2.0](http://getbootstrap.com/).
+* [Font-Awesome - v4.1.0](http://fortawesome.github.io/Font-Awesome/).
 
 And probably more…
--- assets/js/functions.js
+++ assets/js/functions.js
@@ -7,27 +7,6 @@
 //  ----------------------------------------------------------
 
 //  ----------------------------------------------------------
-//  Replace all SVG images with inline SVG
-//  ----------------------------------------------------------
-$('img.svg').each(function(){
-  var $img = $(this);
-  var imgID = $img.attr('id');
-  var imgClass = $img.attr('class');
-  var imgURL = $img.attr('src');
-  $.get(imgURL, function(data) {
-    var $svg = $(data).find('svg');
-    if(typeof imgID !== 'undefined') {
-      $svg = $svg.attr('id', imgID);
-    }
-    if(typeof imgClass !== 'undefined') {
-      $svg = $svg.attr('class', imgClass+' replaced-svg');
-    }
-    $svg = $svg.removeAttr('xmlns:a');
-    $img.replaceWith($svg);
-  }, 'xml');
-});
-
-//  ----------------------------------------------------------
 //  parallax background
 //  ----------------------------------------------------------
 $.stellar({
@@ -37,10 +16,97 @@ $.stellar({
 //  ----------------------------------------------------------
 //  smooth scolling 
 //  ----------------------------------------------------------
-$('#navigation').singlePageNav();
-$('.more').singlePageNav();
+$('#main-navbar').singlePageNav({
+  offset: $('#main-navbar').outerHeight(),
+  updateHash: true,
+});
+$('.more-info').singlePageNav({
+  offset: $('#main-navbar').outerHeight(),
+  updateHash: true,
+});
 
 //  ----------------------------------------------------------
-//  sticky navbar
+//  portfolio filters
 //  ----------------------------------------------------------
-$(".navbar").sticky({topSpacing:0});
+$('#portfolio-grid').mixItUp({
+  animation: {
+    duration: 1000,
+    effects: 'fade scale(0.01) stagger(102ms)',
+    easing: 'cubic-bezier(0.215, 0.61, 0.355, 1)'
+  }
+});
+
+//  ----------------------------------------------------------
+//  contact form validation
+//  ----------------------------------------------------------
+$(function() {
+  $("input,textarea").jqBootstrapValidation({
+    preventSubmit: true,
+    submitError: function($form, event, errors) {},
+    submitSuccess: function($form, event) {
+      event.preventDefault();
+      var name = $("input#name").val();
+      var email = $("input#email").val();
+      var message = $("textarea#message").val();
+      var firstName = name;
+      if (firstName.indexOf(' ') >= 0) {
+        firstName = name.split(' ').slice(0, -1).join(' ');
+      }
+      $.ajax({
+        url: "./contact.php",
+        type: "POST",
+        data: {name: name, email: email, message: message},
+        cache: false,
+        success: function() {
+          $('#success').html("<div class='alert alert-success'>");
+          $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;").append( "</button>");
+          $('#success > .alert-success').append("<strong>Thanks "+firstName+", Your message has been sent. I will get back to you as soon as possible. </strong>");
+          $('#success > .alert-success').append('</div>');
+          $('#contactForm').trigger("reset");
+        },
+        error: function() {
+          $('#success').html("<div class='alert alert-danger'>");
+          $('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;").append( "</button>");
+          $('#success > .alert-danger').append("<strong>Sorry "+firstName+", it seems that my mail server is not responding...</strong> Could you please email me directly to <a href='mailto:sayhi@hezahran.com?Subject=Message_Me from hezahran.com'>sayhi@hezahran.com</a>? Sorry for the inconvenience!");
+          $('#success > .alert-danger').append('</div>');
+          $('#contactForm').trigger("reset");
+        },
+      })
+    },
+    filter: function() {
+      return $(this).is(":visible");
+    },
+  });
+  $("a[data-toggle=\"tab\"]").click(function(e) {
+    e.preventDefault();
+    $(this).tab("show");
+  });
+});
+$('#name').focus(function() {
+  $('#success').html('');
+});
+
+//  ----------------------------------------------------------
+//  google maps
+//  ----------------------------------------------------------
+function initialize() {
+  var myLatlng = new google.maps.LatLng(31.2647696,29.9945858);
+  var mapOptions = {
+    zoom: 16,
+    center: myLatlng,
+    scrollwheel: false
+  }
+  var map = new google.maps.Map(document.getElementById('site-map'), mapOptions);
+  var marker = new google.maps.Marker({
+    position: myLatlng,
+    map: map,
+    title: 'Hello World!'
+  });
+}
+function loadScript() {
+  var script = document.createElement('script');
+  script.type = 'text/javascript';
+  script.src = 'https://maps.googleapis.com/maps/api/js?v=3.exp&' + 'callback=initialize';
+  document.body.appendChild(script);
+}
+window.onload = loadScript;
